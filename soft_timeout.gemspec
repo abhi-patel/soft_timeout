1365038d70a5d216ce03891edba4a1ff2ed05393
Abhishek Patel <abhishek.patel131@gmail.com>
2016-09-29 00:00:52 +0530

    Adding test cases.

--- soft_timeout.gemspec
+++ soft_timeout.gemspec
@@ -11,7 +11,7 @@ Gem::Specification.new do |spec|
 
   spec.summary       = %q{An escape window before Timeout error hits you}
   spec.description   = %q{Provides a better way to handle timeouts in ruby.}
-  spec.homepage      = ""
+  spec.homepage      = "https://github.com/abhi-patel/soft_timeout"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
--- test/soft_timeout_test.rb
+++ test/soft_timeout_test.rb
@@ -1,11 +1,41 @@
 require 'test_helper'
 
 class SoftTimeoutTest < Minitest::Test
+  def setup
+    @soft_timeout = SoftTimeout::Timeout.new(1,3) do
+      print 'print on soft timeout'
+    end
+
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::SoftTimeout::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_soft_timeout_initialize
+    assert_equal @soft_timeout.soft_expiry, 1
+    assert_equal @soft_timeout.hard_expiry, 3
+    assert_equal @soft_timeout.error_class, ::Timeout::Error
+  end
+
+  def test_should_execute_soft_timeout_proc
+    out, err = capture_io do
+      @soft_timeout.soft_timeout{ sleep(2)}
+    end
+    assert_equal "print on soft timeout", out
+  end
+
+
+  def test_should_not_execute_soft_timeout_proc
+    out, err = capture_io do
+      @soft_timeout.soft_timeout{ sleep(1)}
+    end
+    assert_equal '', out
+  end
+
+  def test_should_raise_error
+    assert_raises ::Timeout::Error do
+      @soft_timeout.soft_timeout{ sleep(4)}
+    end
   end
 end
